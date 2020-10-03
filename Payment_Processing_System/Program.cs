8079e847cfeb017d17e9d91f880d5098c39ba5ca
Jemini Mehta <Jemini.Mehta@jet2.com>
2020-10-03 20:35:00 +0530

    added comments.

--- Payment_Processing_System/Program.cs
+++ Payment_Processing_System/Program.cs
@@ -12,22 +12,27 @@ namespace Payment_Processing_System
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
 
+            //Display rule for PhysicalProduct.
             BusinessRulesEngineService businessRulesEngine = new BusinessRulesEngineService(new PhysicalProduct());
             string rule = businessRulesEngine.HandleOrder();
             Console.WriteLine("Rule for Physical Product is " + rule);
 
+            //Display rule for Book.
             businessRulesEngine = new BusinessRulesEngineService(new Book());
             rule = businessRulesEngine.HandleOrder();
             Console.WriteLine("Rule for Book is " + rule);
 
+            //Display rule for MemberShip.
             businessRulesEngine = new BusinessRulesEngineService(new MemberShip());
             rule = businessRulesEngine.HandleOrder();
             Console.WriteLine("Rule for MemberShip is " + rule);
 
+            //Display rule for Upgrade to a MemberShip.
             businessRulesEngine = new BusinessRulesEngineService(new UpgradeMemberShip());
             rule = businessRulesEngine.HandleOrder();
             Console.WriteLine("Rule for UpgradeMemberShip is " + rule);
 
+            //Display rule for  video “Learning to Ski”.
             businessRulesEngine = new BusinessRulesEngineService(new VideoLearningtoSki());
             rule = businessRulesEngine.HandleOrder();
             Console.WriteLine("Rule for Video \"Learning to Ski\" is " + rule);
